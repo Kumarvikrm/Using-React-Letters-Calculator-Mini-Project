408b3b120d7ee14076bd276d30d3bb8da2a2f27e
Kumarvikrm <kumarvikram68715@gmail.com>
2025-03-07 17:55:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LettersCalculator from './Components/LettersCalculator';
+
+const App = () => {
+  return (
+    <>
+      <LettersCalculator/>
+    </>
+  )
+}
+
+export default App
